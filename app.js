32a1e1ea695388b26c9c3c772945219e36756962
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-09-26 16:15:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// string kiritiladi string ichidagi + larni 0 ga aylantishish ⇒ “— + — +” ⇒ “— 0 — 0”
+
+// let str = " - + - +";
+
+// console.log(str.replace(`- + - +`, `- 0 - 0`));
+
+// functionga arrey beriladi va bitta son beriladi shu sondan katta bo’lgan sonlarni o’chirib bersin
+
+// let arr = [10, 3, 50, 20, 6, 7]
+
+// let num = [10]
+
+// function Numbers(arr, num){
+//     return arr.filter (item => item <= num)
+// }
+
+// let newArr = Numbers(arr, num)
+
+// console.log(arr);
+// console.log(newArr);
+
+
+// functionga son beriladi 1 honali ⇒ 1 * 5 = 5 ,2 * 5 = 10
+// functionga son beriladi va shundan kichik sonlar ni arreyga solib qaytaradi
+
+// let arr = [10, 3, 50, 20, 6, 7]
+
+// let num = [10]
+
+// function Numbers(arr, num){
+//     return arr.filter (item => item <= num)
+// }
+
+// let newArr = Numbers(arr, num)
+
+// console.log(newArr);
+
+
+// functionga son beriladi shu sondan kichik bo’gan tub sonlarni arreyga solib qaytarsin
+
+// let tub = [1, 2, 3, 7, 13, 17, 19]
+
+// let num = [7]
+
+// function isPrime (tub){
+//     if(tub <= 1) return false
+
+//     for(let i = 0; i <= Math.sqrt(tub); i++){
+//         if (tub % 2 === 0)return false
+//     }
+
+//     return true
+// }
+
+// function newTub(arr){
+//   return arr.filter(isPrime)
+// }
+
+// let primes = newTub(tub)
+
+// console.log("kichik tub sonlar", primes);
+
+
+
